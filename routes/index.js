26d56f1b7cd58bbb4bfbd0893f18142d9dca036b
Emma <emalysz@usc.edu>
2019-02-10 19:51:03 -0800

    new additions

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Leadership as an Artform' });
+});
+
+/* GET signup page for executive. */
+router.get('/', function(req, res, next) {
+  res.render('executiveSignup', { title: 'Executive Signup' });
 });
 
 module.exports = router;
