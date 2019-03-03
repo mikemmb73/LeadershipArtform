ea32efb06f1c9a1334b91fd9f14b463fe08d0aaa
Nicole <njbergma@usc.edu>
2019-03-03 13:06:31 -0800

    working with passport

--- routes/passport.js
+++ routes/passport.js
@@ -11,15 +11,21 @@ var LocalStrategy = require('passport-local').Strategy;
 //     });
 
 /* POST login page. */
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) return next(err)
-    if (!user) {
-      return res.redirect('/error')
-    }
-    req.login(user, function(err) {
-      if (err) return next(err);
-      return res.redirect('/');
-    });
-  })(req, res, next);
-});
+router.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: true })
+);
+
+// router.post('/login', function(req, res, next) {
+//   passport.authenticate('local', function(err, user, info) {
+//     if (err) return next(err)
+//     if (!user) {
+//       return res.redirect('/error')
+//     }
+//     req.login(user, function(err) {
+//       if (err) return next(err);
+//       return res.redirect('/');
+//     });
+//   })(req, res, next);
+// });
