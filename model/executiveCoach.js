ae606f44c488eed5acaa847e4c1a0897a1f097cc
Caroline Chocholak <carolinechocholak@Carolines-MacBook-Pro.local>
2019-03-03 19:25:52 -0800

    adding sign up for coach

--- model/executiveCoach.js
+++ model/executiveCoach.js
@@ -1,69 +1,112 @@
 class ExecutiveCoach {
- 	constructor(fname, lname, email, phoneNumber, password, bio, photo) {
+
+  // constructor(obj) {
+  //   this.fname = obj.fname;
+  //   this.lname = obj.lname;
+  //   this.email = obj.email;
+  //   this.phone_number = obj.phone_number;
+  //   this.password = obj.password;
+  //   this.bio = obj.bio;
+  //   this.photo = obj.photo;
+  //   this.coach_id = obj.coach_id;
+  //
+  //   var goals = [];
+  //   this.goals = goals;
+  //
+  //   var executives = [];
+  //   this.executives = executives;
+  // }
+  //
+ 	constructor(fname, lname, email, phone_number, password, bio, photo, coach_id) {
 		this.fname = fname;
 		this.lname = lname;
-		this.email = email,
-		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.phone_number = phone_number;
 		this.password = password;
 		this.bio = bio;
-		this.photo = photo; 
+		this.photo = photo;
+    this.coach_id = coach_id;
 
 		var goals = [];
-		this.goals = goals; 
+		this.goals = goals;
 
 		var executives = [];
-		this.executives = executives; 
+		this.executives = executives;
 
 		//assign an ID to the executive coach
 		//idea: have a global counter that increments by 1 every time a coach signs up
 
-		var id = controller.getNextAvailableID(); 
-		this.id = id; 
+		// var id = controller.getNextAvailableID();
+		// this.id = id;
 
 	}
-
-	get name() {
-		return this.fname + " " + this.lname; 
+  set phoneNumber(phone_number) {
+    this.phone_number = phone_number;
+  }
+  set lname(lname) {
+		this.lname = lname;
 	}
+  set fname(fname) {
+    this.fname = fname;
+  }
 
-	get username() {
-		return this.email; 
+	get email() {
+		return this.email;
 	}
+  set email(email) {
+    this.email = email;
+  }
 
 	get phoneNumber() {
-		return this.phoneNumber; 
+		return this.phone_number;
 	}
 
 	get password() {
-		return this.password; 
+		return this.password;
 	}
+  set password(password) {
+    this.password = password;
+  }
 
 	get bio() {
-		return this.bio; 
+		return this.bio;
 	}
+  set bio(bio) {
+    this.bio = bio;
+  }
 
 	get photo() {
-		return this.photo; 
+		return this.photo;
 	}
+
+  set photo(photo) {
+    this.photo = photo;
+  }
 
 	get goals() {
 		return this.goals;
 	}
 
-	get id() {
-		return this.id; 
+	get coach_id() {
+		return this.coach_id;
 	}
+  set coach_id(coach_id) {
+    this.coach_id = coach_id;
+  }
 
 	get executives() {
-		return this.executives; 
+		return this.executives;
 	}
 
 	addGoal(goal) {
-		this.goals.push(goal); 
+		this.goals.push(goal);
 	}
 
 	addExecutive(executive) {
-		//this function needs to be called when an executive signs up 
+		//this function needs to be called when an executive signs up
 		this.executives.push(executive);
 	}
 }
+module.exports = {
+  ExecutiveCoach
+};
