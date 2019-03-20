666f6955eb38c00695589108b4309891a95a81cc
Nicole <njbergma@usc.edu>
2019-03-20 14:42:38 -0700

    simple user auth

--- services/loginservices.js
+++ services/loginservices.js
@@ -9,10 +9,13 @@ module.exports = {
 	    const [rows, fields] = await mysql.connect.execute("SELECT * FROM executives WHERE email = ?", [email.toLowerCase()]);
 	    if (rows != null) {
 	      if (rows.length != 0) {
-	        const [rows, fields] = await mysql.connect.execute("SELECT * FROM executives where email = ?", [email.toLowerCase()]);
 	        const currExecutive = rows.map(x => new Executive.Executive(x));
 	        currentExecutive = currExecutive[0];
-	        return currExecutive[0];
+					var pw = currentExecutive.pass;
+					if (pw == password) {
+						return currExecutive[0];
+					}
+					else console.log("PASSWORD DOESNT MATCH");
 	      }
 	    }
 	    return null;
@@ -20,12 +23,15 @@ module.exports = {
 
 	getCoachAuthent: async function(email, password) {
 	    const [rows, fields] = await mysql.connect.execute("SELECT * FROM coaches WHERE email = ?", [email.toLowerCase()]);
-	    if (rows != null) {
-	      if (rows.length != 0) {
-	        const [rows, fields] = await mysql.connect.execute("SELECT * FROM coaches WHERE email = ?", [email.toLowerCase()]);
+	    if (rows != null) {		// textbox not empty
+	      if (rows.length != 0) {		// email entry found
 	        const currCoach = rows.map(x => new ExecutiveCoach.ExecutiveCoach(x));
 	        currentCoach = currCoach[0];
-	        return currCoach[0];
+					var pw = currentCoach.pass;
+					if (pw == password) {		// password entered = password in db
+						return currCoach[0];
+					}
+					else console.log("PASSWORD DOESNT MATCH");
 	      }
 	    }
 	    return null;
@@ -33,8 +39,8 @@ module.exports = {
 
 	getClients: async function(user) {
 	    var id = user.coach_id_val;
-	    console.log("ID HERE" + id);
-	    var getStatement = "SELECT * FROM executives WHERE coach_id = IFNULL(" + id + ", coach_id)";
+	    console.log("ID HERE: " + id);
+	    var getStatement = "SELECT * FROM executives WHERE coach_id = IFNULL(" + user.coach_id_val + ", coach_id)";
 	    const [rows, fields] = await mysql.connect.execute(getStatement);
 	    const currCoach = rows.map(x => new Executive.Executive(x));
 	    return currCoach;
