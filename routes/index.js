e236c036b02fea9c13a7e9a3c9eb5f1bdbedd92e
Emma <emalysz@usc.edu>
2019-03-25 20:16:05 -0700

    prev goal feature

--- routes/index.js
+++ routes/index.js
@@ -85,6 +85,16 @@ router.post('/coachView', async function(req, res) {
       }
   } else if (req.body.emailReminder != null) {
       emailServices.sendOneReminder(req.body.emailReminder);
+  } else if (req.body.addCoachGoal != null) {
+    console.log("coach's goal added"); 
+    var data2 = qs.parse(req.body);
+    console.log(data2); 
+    if (data2.goalTitle != "") {
+      addGoalService.addGoalCoach(data2, currCoach, clients); 
+    } else {
+      addGoalService.addPrevGoal(data2, req.body.GoalButton, currCoach, clients); 
+    }
+    res.render('coachView.pug', {title: 'CoachView', user: currCoach, clients: clients});
   } else {
       console.log("otherwise i'm here");
       var name = req.body.clientName;
@@ -221,26 +231,11 @@ router.post('/editGoal_executive', async function(req, res) {
   //res.send("currExecutive's goal length " + currExecutive.goals_list.length);
 });
 
-router.post('/viewGoal', function(req, res) {
-  console.log("posting to view goal"); 
-  if (req.body.addCoachGoal != null) {
-    console.log("coach's goal added"); 
-    var data2 = qs.parse(req.body);
-    console.log(data2); 
-    if (data2.goalTitle != "") {
-      addGoalService.addGoalCoach(data2, currCoach, clients); 
-    } else {
-      console.log("we are using a previous goal"); 
-      addPrevGoal(data2, currCoach, clients); 
-    }
-    res.render('viewGoal.pug', {title: 'View Goal', user: currExecutive}); 
-  } else {
-    var data = qs.parse(req.body);
-    addGoalService.addGoalExecutive(data, currExecutive);
-    console.log(data);
-    res.send("currExecutive's goal length " + currExecutive.goals_list.length);
-  }
-  //console.log(req.body["mcQuestions[0]"]);
+router.post('/viewGoal_executive', function(req, res) {
+  var data = qs.parse(req.body);
+  addGoalService.addGoalExecutive(data, currExecutive);
+  console.log(data);
+  res.send("currExecutive's goal length " + currExecutive.goals_list.length);
 });
 
 
