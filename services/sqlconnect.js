b38f8177b13cb1caaa0740d1490b76255c96541c
Caroline Chocholak <carolinechocholak@carolines-mbp.home>
2019-04-09 01:52:10 -0700

    adding database configuration

--- services/sqlconnect.js
+++ services/sqlconnect.js
@@ -1,11 +1,12 @@
 var mysql2 = require('mysql2/promise');
 
-const connection = mysql2.createPool({
+var connection = mysql.createPool({
   connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'rootroot',
-  database : 'Leadership_Artform'
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DB_NAME
 });
 
 
