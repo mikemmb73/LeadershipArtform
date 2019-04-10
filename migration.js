e4c2cbbf67121bb783e249c1531c8c2814766fca
Caroline Chocholak <carolinechocholak@carolines-mbp.home>
2019-04-09 23:45:06 -0700

    migration.js changes

--- migration.js
+++ migration.js
@@ -3,11 +3,11 @@ var migration = require('mysql-migrations');
 
 var connection = mysql.createPool({
   connectionLimit : 10,
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database : process.env.RDS_DB_NAME
+  host     : "leadershipartform.cc8lws3esn4q.us-west-2.rds.amazonaws.com",
+  user     : "artOfLeadership",
+  password : "capstone455",
+  port     : "3306",
+  database : "leader_artform_db"
 });
 
 
