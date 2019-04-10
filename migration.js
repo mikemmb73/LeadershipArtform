0790a5c3b76bade03f6b1bdeef51c8098dec9d94
Jen Anthony <anth595@usc.edu>
2019-04-09 18:23:01 -0700

    no pending approvals message on coachView

--- migration.js
+++ migration.js
@@ -1,13 +1,12 @@
 var mysql = require('mysql2');
 var migration = require('mysql-migrations');
 
-var connection = mysql.createPool({
+const connection = mysql2.createPool({
   connectionLimit : 10,
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT,
-  database : process.env.RDS_DB_NAME
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'Leadership_Artform'
 });
 
 
