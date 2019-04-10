86cf458ebebe46c7b5822cae83bde0353552fa43
Jen Anthony <anth595@usc.edu>
2019-04-10 16:50:49 -0700

    commented out sql stuff and commented back in env stuff

--- migration.js
+++ migration.js
@@ -1,21 +1,21 @@
 var mysql = require('mysql2');
 var migration = require('mysql-migrations');
 
-// var connection = mysql.createPool({
-//   connectionLimit : 10,
-//   host     : process.env.RDS_HOSTNAME,
-//   user     : process.env.RDS_USERNAME,
-//   password : process.env.RDS_PASSWORD,
-//   port     : process.env.RDS_PORT,
-//   database : process.env.RDS_DB_NAME
-// });
-
 var connection = mysql.createPool({
   connectionLimit : 10,
-  host     : 'localhost',
-  user     : 'root',
-  password : 'Pickoftheweek1!',
-  database : 'Leadership_Artform'
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DB_NAME
 });
+
+// var connection = mysql.createPool({
+//   connectionLimit : 10,
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'Pickoftheweek1!',
+//   database : 'Leadership_Artform'
+// });
 
 migration.init(connection, __dirname + '/migrations');
