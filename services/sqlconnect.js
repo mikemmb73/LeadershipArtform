7ee7bdb1b3f543a081910e517a661a8e69955d9a
Jen Anthony <anth595@usc.edu>
2019-04-10 16:40:42 -0700

    you have new goal and images on coachProfile_exec and coachView

--- services/sqlconnect.js
+++ services/sqlconnect.js
@@ -1,14 +1,20 @@
 var mysql2 = require('mysql2/promise');
 
+// var connection = mysql2.createPool({
+//   connectionLimit : 10,
+//   host     : "localhost",
+//   user     : "root",
+//   password : "Chalked1512!",
+//   database : "Leadership_Artform"
+// });
+
 var connection = mysql2.createPool({
   connectionLimit : 10,
-  host     : "localhost",
-  user     : "root",
-  password : "Chalked1512!",
-  database : "Leadership_Artform"
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Pickoftheweek1!',
+  database : 'Leadership_Artform'
 });
-
-
 
 
 module.exports = {
