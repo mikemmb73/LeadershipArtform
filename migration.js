33d1f8d08362a3ab1f0932710d212bfd8128830d
canchen <canchen@usc.edu>
2021-03-02 00:32:14 +0800

    Revert "added an option for not setting the reminder"
    This reverts commit c401e61aea0e1bd35083c30d3de136d94a158e92.

---
+++ migration.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql2');
+var migration = require('mysql-migrations');
+
+/*
+If choosing to host on the actual website, use these environmental variables
+(specified in the .env file) to access AWS.
+*/
+
+var connection = mysql.createPool({
+  connectionLimit : 10,
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database : process.env.RDS_DB_NAME
+});
+
+
+/*
+If choosing to host locally, use your mySQL credentials
+*/
+
+// var connection = mysql.createPool({
+//   connectionLimit : 10,
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'Pickoftheweek1!',
+//   database : 'Leadership_Artform'
+// });
+
+
+migration.init(connection, __dirname + '/migrations');
