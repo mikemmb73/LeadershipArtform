701e8bf60e3940f9436cc47f5cc5cc0dd0c68a03
Michael Baldwin <mikemmb73@gmail.com>
2021-03-09 16:21:34 -0800

    added tests for exec login

--- test/loginServicesTest.js
+++ test/loginServicesTest.js
@@ -1,6 +1,7 @@
 const { signUpCoach } = require('../services/signup.js')
 const { signUpExecutive } = require('../services/signup.js')
 const { getCoachAuthent } = require('../services/loginservices.js')
+const { getExecutiveAuthent } = require('../services/loginservices.js')
 var expect = require('chai').expect;
 var mysql = require("../services/sqlconnect.js");
 var ExecutiveCoach = require('../model/executiveCoach');
@@ -13,7 +14,7 @@ describe('Login Coach', function() {
 
     before(async function(){
         con = mysql.connect;
-        await con.execute("DELETE FROM executives WHERE email = ?", [email]);
+        await con.execute("DELETE FROM coaches WHERE email = ?", [email]);
         var coach = await signUpCoach("fname", "lname", email, "2066968408", pass, pass, "bio", null)
     })
 
@@ -31,6 +32,55 @@ describe('Login Coach', function() {
         it("it does not allow the user to login", async function(){
             var loginResponse = await getCoachAuthent(email, "incorrectPassword")
 
+            expect(loginResponse).to.be.equal(null);
+        })
+    })
+})
+
+
+describe('Login Executive', function() {
+    var con = null;
+    var email = "email@email.com"
+    var pass = "password"
+    var coachId = null;
+
+    before(function(done){
+        con = mysql.connect;
+        con.execute("DELETE FROM executives WHERE email = ?", [email]);
+        con.execute("DELETE FROM coaches WHERE email = ?", [email]);
+        var coach = signUpCoach("fname", "lname", email, "2066968408", pass, pass, "bio", null)
+        done();
+
+        coachId = coach.coach_id_val();
+    });
+
+    beforeEach(function(done){
+        signUpExecutive("fname", "lname", email, "2066968408", pass, pass, "bio", null, coachId)
+        done();
+    });
+
+    //after each test, remove the account we created so it does not interfere with other tests
+    afterEach(function(done) {
+        con.execute("DELETE FROM executives WHERE email = ?", ["email@email.com"]);
+        
+        done();
+    });
+
+    context("The user attempts a valid login", function(){
+        it("allows the user to login", function(done){
+            var loginResponse = getExecutiveAuthent(email, pass)
+            done();
+
+            expect(loginResponse).to.be.object();
+            expect(loginResponse.username).to.be.a('string').and.equal("email@email.com")
+        })
+    })
+
+    context("The user attempts an invalid login", function(){
+        it("it does not allow the user to login", function(done){
+            var loginResponse = getExecutiveAuthent(email, "incorrectPassword")
+            done();
+            
             expect(loginResponse).to.be.equal(null);
         })
     })
