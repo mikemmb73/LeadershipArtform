beaa80afea437cfbc6d5027a2a48680e7009d354
Michael Baldwin <mikemmb73@gmail.com>
2021-03-21 17:29:53 -0700

    goal tests

---
+++ test/goalTest.js
@@ -0,0 +1,10 @@
+var loginservices = require('../services/loginservices.js')
+const { signUpCoach } = require('../services/signup.js')
+const { signUpExecutive } = require('../services/signup.js')
+var loginservices = require('../services/signup.js')
+var expect = require('chai').expect;
+var mysql = require("../services/sqlconnect.js");
+var ExecutiveCoach = require('../model/executiveCoach');
+var Executive = require('../model/executive');
+
+
